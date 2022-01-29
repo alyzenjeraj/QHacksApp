32d24a05f23981aa2103ada9c86dbf2b263d668d
Alyzen Jeraj <alyzenjeraj@gmail.com>
2022-01-29 13:43:33 -0500

    login page added

--- app/screens/loginScreen.js
+++ app/screens/loginScreen.js
@@ -1,16 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TextInput, TouchableOpacity, } from 'react-native';
 
 function loginScreen(props) {
     return (
-        <SafeAreaView>
-            <Text> peep </Text>
+        <SafeAreaView style={styles.mainView}>
+            <Image style={styles.image} source={require('../assets/Finch_2.png')} />
+
+            <View style={styles.ipview}>
+                <TextInput placeholder="Email" 
+                style={styles.inputter}
+                placeholderTextColor="white"/>
+            </View>
+            <View style={styles.ipview}>
+                <TextInput placeholder="Password" 
+                style={styles.inputter}
+                placeholderTextColor="white"/>
+            </View>
+            <TouchableOpacity>
+                <Text>Forgot Password?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.logBot}>
+                <Text>LOGIN</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
-    
+    image: {
+        width: 200,
+        height: 200,
+        position: 'relative',
+        bottom: 0,
+        alignSelf: 'center',
+    },
+    space: {
+        width: 20,
+        height: 20,
+    },
+    inputter: {
+        flex: 1,
+        alignSelf: 'center',
+        height: 50,
+    },
+
+    ipview: {
+        backgroundColor: '#f578e5',
+        opacity: 30,
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        margin: 15,
+        alignSelf: 'center'
+    },
+    mainView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+    },
+    logBot: {
+        padding: 20,
+    }
 })
 
 export default loginScreen;
