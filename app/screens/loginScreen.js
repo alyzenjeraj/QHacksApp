1e459aebadd7ecf278ae499fc9782b90d45ee29a
Alyzen Jeraj <alyzenjeraj@gmail.com>
2022-01-29 00:07:36 -0500

    initial setup

---
+++ app/screens/loginScreen.js
@@ -0,0 +1,20 @@
+rsf
+import React from 'react';
+
+function loginScreen(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+rnss
+const styles = StyleSheet.create({
+    
+})
+
+export default loginScreen;
+
+
+
